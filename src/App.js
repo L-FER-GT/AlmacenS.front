16417210a43907c34bab35a6dd03d196ed92cfc2
Fernando <GutierrezT.LFernando@gmail.com>
2024-01-31 10:58:20 -0500

    coneccion con el backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Home from './view/screen/home'; // Ajusta la ruta según la ubicación de tu componente Home
 import Login from './view/screen/loginScreen';
+import axios from 'axios';
 import './App.css';
+
+// import bcrypt from 'bcrypt'
+// const bcrypt = require('bcrypt');
 
 function App() {
   const [screenSelect, setScreenSelect]=useState('Home');
+  const [trabajadores, setTrabajadores] = useState([])
+
+  useEffect(() => {
+    obtenerTrabajadores();
+  }, []);
+  useEffect(()=>{console.log(trabajadores)},[trabajadores]);
+
+  const obtenerTrabajadores = () => {
+    axios.get('http://localhost:5000/trabajadores')
+      .then((response) => {
+        setTrabajadores(response.data);
+        let auxValue=response.data[0];
+        // DetectarPasswords('12345',auxValue[Contrasena]);
+      })
+      .catch((error) => {
+        console.error('Error al obtener datos desde el servidor: ', error);
+      });
+  };
+
+  // const DetectarPasswords=(passwordFromUser,hashedPasswordFromDatabase)=>{
+  //   bcrypt.compare(passwordFromUser, hashedPasswordFromDatabase, (err, result) => {
+  //     if (err) {
+  //       console.error("Error al comparar contraseñas: ", err);
+  //       // Manejar el error
+  //     } else {
+  //       if (result) {
+  //         console.log("La contraseña es válida");
+  //         // Permitir el acceso
+  //       } else {
+  //         console.log("La contraseña no es válida");
+  //         // Denegar el acceso
+  //       }
+  //     }
+  //   });
+  // }
+
   return (
     <div className="App">
       {/* Contenido del componente Home */}
